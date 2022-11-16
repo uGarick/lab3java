a1d410e136c7022168415aadb51126a4c0202584
Igor <igor.gaz2004@gmail.com>
2022-11-16 13:32:49 +0300

    lab 3

---
+++ src/Location.java
@@ -0,0 +1,46 @@
+import java.util.Objects;
+
+/**
+ * This class represents a specific location in a 2D map.  Coordinates are
+ * integer values.
+ **/
+public class Location
+{
+    /** X coordinate of this location. **/
+    public int xCoord;
+
+    /** Y coordinate of this location. **/
+    public int yCoord;
+
+
+    /** Creates a new location with the specified integer coordinates. **/
+    public Location(int x, int y)
+    {
+        xCoord = x;
+        yCoord = y;
+    }
+
+    /** Creates a new location with coordinates (0, 0). **/
+    public Location()
+    {
+        this(0, 0);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+
+        if (obj instanceof Location) {
+            Location other = (Location) obj;
+            if (xCoord == other.xCoord && yCoord == other.yCoord) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(xCoord, yCoord);
+    }
+}
